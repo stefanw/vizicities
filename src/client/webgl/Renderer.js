002bd4d81b174024e8de49dc4f12757ba64c5c1b
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2015-01-19 15:42:32 +0100

    Use StereoEffect Renderer

--- src/client/webgl/Renderer.js
+++ src/client/webgl/Renderer.js
@@ -1,4 +1,137 @@
 /* globals window, _, VIZI, THREE */
+
+THREE.StereoEffect = function ( renderer ) {
+
+	// API
+
+	var scope = this;
+
+	this.eyeSeparation = 3;
+	this.focalLength = 15;	// Distance to the non-parallax or projection plane
+
+	Object.defineProperties( this, {
+		separation: {
+			get: function () {
+				return scope.eyeSeparation;
+			},
+			set: function ( value ) {
+				console.warn( 'THREE.StereoEffect: .separation is now .eyeSeparation.' );
+				scope.eyeSeparation = value;
+			}
+		},
+		targetDistance: {
+			get: function () {
+				return scope.focalLength;
+			},
+			set: function ( value ) {
+				console.warn( 'THREE.StereoEffect: .targetDistance is now .focalLength.' );
+				scope.focalLength = value;
+			}
+		}
+	} );
+
+	// internals
+
+	var _width, _height;
+
+	var _position = new THREE.Vector3();
+	var _quaternion = new THREE.Quaternion();
+	var _scale = new THREE.Vector3();
+
+	var _cameraL = new THREE.PerspectiveCamera();
+	var _cameraR = new THREE.PerspectiveCamera();
+
+	var _fov;
+	var _outer, _inner, _top, _bottom;
+	var _ndfl, _halfFocalWidth, _halfFocalHeight;
+	var _innerFactor, _outerFactor;
+
+	// initialization
+
+	renderer.autoClear = false;
+
+	this.setSize = function ( width, height ) {
+
+		_width = width / 2;
+		_height = height;
+
+		renderer.setSize( width, height );
+
+	};
+
+	this.render = function ( scene, camera ) {
+
+		scene.updateMatrixWorld();
+
+		if ( camera.parent === undefined ) camera.updateMatrixWorld();
+
+		camera.matrixWorld.decompose( _position, _quaternion, _scale );
+
+		// Effective fov of the camera
+
+		_fov = THREE.Math.radToDeg( 2 * Math.atan( Math.tan( THREE.Math.degToRad( camera.fov ) * 0.5 ) / camera.zoom ) );
+
+		_ndfl = camera.near / this.focalLength;
+		_halfFocalHeight = Math.tan( THREE.Math.degToRad( _fov ) * 0.5 ) * this.focalLength;
+		_halfFocalWidth = _halfFocalHeight * 0.5 * camera.aspect;
+
+		_top = _halfFocalHeight * _ndfl;
+		_bottom = -_top;
+		_innerFactor = ( _halfFocalWidth + this.eyeSeparation / 2.0 ) / ( _halfFocalWidth * 2.0 );
+		_outerFactor = 1.0 - _innerFactor;
+
+		_outer = _halfFocalWidth * 2.0 * _ndfl * _outerFactor;
+		_inner = _halfFocalWidth * 2.0 * _ndfl * _innerFactor;
+
+		// left
+
+		_cameraL.projectionMatrix.makeFrustum(
+			-_outer,
+			_inner,
+			_bottom,
+			_top,
+			camera.near,
+			camera.far
+		);
+
+		_cameraL.position.copy( _position );
+		_cameraL.quaternion.copy( _quaternion );
+		_cameraL.translateX( - this.eyeSeparation / 2.0 );
+
+		// right
+
+		_cameraR.projectionMatrix.makeFrustum(
+			-_inner,
+			_outer,
+			_bottom,
+			_top,
+			camera.near,
+			camera.far
+		);
+
+		_cameraR.position.copy( _position );
+		_cameraR.quaternion.copy( _quaternion );
+		_cameraR.translateX( this.eyeSeparation / 2.0 );
+
+		//
+
+		renderer.clear();
+		renderer.enableScissorTest( true );
+
+		renderer.setScissor( 0, 0, _width, _height );
+		renderer.setViewport( 0, 0, _width, _height );
+		renderer.render( scene, _cameraL );
+
+		renderer.setScissor( _width, 0, _width, _height );
+		renderer.setViewport( _width, 0, _width, _height );
+		renderer.render( scene, _cameraR );
+
+		renderer.enableScissorTest( false );
+
+	};
+
+};
+
 (function() {
 	"use strict";
 
@@ -19,23 +152,27 @@
 	};
 
 	VIZI.Renderer.prototype.createRenderer = function() {
-		var renderer = new THREE.WebGLRenderer({
+		var normal_renderer = new THREE.WebGLRenderer({
 			antialias: false
 		});
 
-		renderer.setSize( window.innerWidth, window.innerHeight );
-		renderer.setClearColor( this.scene.fog.color, 1 );
+		normal_renderer.setSize( window.innerWidth, window.innerHeight );
+		// renderer.setClearColor( this.scene.fog.color, 1 );
 
 		// Gamma settings make things look 'nicer' for some reason
-		renderer.gammaInput = true;
-		renderer.gammaOutput = true;
+		normal_renderer.gammaInput = true;
+		normal_renderer.gammaOutput = true;
 
-		renderer.physicallyBasedShading = true;
+		normal_renderer.physicallyBasedShading = true;
 
-		renderer.shadowMapEnabled = true;
-		renderer.shadowMapSoft = true;
+		normal_renderer.shadowMapEnabled = true;
+		normal_renderer.shadowMapSoft = true;
 
-		this.domContainer.appendChild(renderer.domElement);
+		this.domContainer.appendChild(normal_renderer.domElement);
+
+
+		var renderer = new THREE.StereoEffect( normal_renderer, {worldScale: 100} );
+		renderer.setSize( window.innerWidth, window.innerHeight );
 
 		return renderer;
 	};
